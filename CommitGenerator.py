8a8d1ba94d83ea8573389f2eea2d355230940d71
Rafał 147719 <chx101374@student.chorzow.merito.pl>
2024-08-04 17:54:44 +0200

    poprawienie kodu

--- CommitGenerator.py
+++ CommitGenerator.py
@@ -1,5 +1,17 @@
 import time
 import subprocess
+import json
+from datetime import datetime
+
+CONFIG_FILE = 'config.json'
+
+def load_config():
+    with open(CONFIG_FILE, 'r') as file:
+        return json.load(file)
+
+def save_config(config):
+    with open(CONFIG_FILE, 'w') as file:
+        json.dump(config, file)
 
 def add_dot_to_readme(file_path):
     with open(file_path, 'a') as file:
@@ -12,10 +24,20 @@ def commit_and_push_changes():
 
 def main():
     file_path = 'ReadMe.txt'
-    while True:
+    config = load_config()
+
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    if config['last_commit_date'] != current_date:
+        config['last_commit_date'] = current_date
+        config['commit_count'] = 0
+
+    if config['commit_count'] < 5:
         add_dot_to_readme(file_path)
         commit_and_push_changes()
-        time.sleep(600)  # Czeka 600 sekund (10 minut)
+        config['commit_count'] += 1
+        save_config(config)
+    else:
+        print("Reached maximum number of commits for today.")
 
 if __name__ == '__main__':
     main()
