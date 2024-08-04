fdec1c062e71b24bed96d2a4e39bff18c7336d4c
Rafał 147719 <chx101374@student.chorzow.merito.pl>
2024-08-04 17:32:13 +0200

    Aktualizacja

--- CommitGenerator.py
+++ CommitGenerator.py
@@ -1,1 +1,21 @@
+import time
+import subprocess
 
+def add_dot_to_readme(file_path):
+    with open(file_path, 'a') as file:
+        file.write('.')
+
+def commit_and_push_changes():
+    subprocess.run(['git', 'add', 'ReadMe.txt'])
+    subprocess.run(['git', 'commit', '-m', 'Add dot to ReadMe'])
+    subprocess.run(['git', 'push'])
+
+def main():
+    file_path = 'ReadMe.txt'
+    while True:
+        add_dot_to_readme(file_path)
+        commit_and_push_changes()
+        time.sleep(600)  # Czeka 600 sekund (10 minut)
+
+if __name__ == '__main__':
+    main()
