eef8434741b59676d0286b453cb6b8e025c82572
Rafał 147719 <chx101374@student.chorzow.merito.pl>
2024-08-05 18:23:47 +0200

    Wprowadzenie 1h działania kodu oraz zmiana interwalu na 10min

--- CommitGenerator.py
+++ CommitGenerator.py
@@ -2,12 +2,21 @@ import time
 import subprocess
 import json
 from datetime import datetime
+import os
 
 CONFIG_FILE = 'config.json'
+RUN_DURATION = 3600  # Czas dzia³ania skryptu w sekundach (1 godzina)
+INTERVAL = 600       # Czas miêdzy dodawaniem kropek w sekundach (10 minut)
 
 def load_config():
-    with open(CONFIG_FILE, 'r') as file:
-        return json.load(file)
+    if not os.path.exists(CONFIG_FILE):
+        # Jeœli plik konfiguracyjny nie istnieje, stwórz nowy z domyœlnymi wartoœciami
+        config = {"last_commit_date": "", "commit_count": 0}
+        save_config(config)
+    else:
+        with open(CONFIG_FILE, 'r') as file:
+            config = json.load(file)
+    return config
 
 def save_config(config):
     with open(CONFIG_FILE, 'w') as file:
@@ -26,18 +35,25 @@ def main():
     file_path = 'ReadMe.txt'
     config = load_config()
 
-    current_date = datetime.now().strftime('%Y-%m-%d')
-    if config['last_commit_date'] != current_date:
-        config['last_commit_date'] = current_date
-        config['commit_count'] = 0
+    # Rejestracja czasu rozpoczêcia dzia³ania
+    start_time = time.time()
+    
+    while time.time() - start_time < RUN_DURATION:
+        current_date = datetime.now().strftime('%Y-%m-%d')
+        if config['last_commit_date'] != current_date:
+            config['last_commit_date'] = current_date
+            config['commit_count'] = 0
 
-    if config['commit_count'] < 5:
-        add_dot_to_readme(file_path)
-        commit_and_push_changes()
-        config['commit_count'] += 1
-        save_config(config)
-    else:
-        print("Reached maximum number of commits for today.")
+        if config['commit_count'] < 5:
+            add_dot_to_readme(file_path)
+            commit_and_push_changes()
+            config['commit_count'] += 1
+            save_config(config)
+        else:
+            print("Reached maximum number of commits for today.")
+        
+        # Czekaj przez 10 minut przed kolejnym dodaniem kropki
+        time.sleep(INTERVAL)
 
 if __name__ == '__main__':
     main()
